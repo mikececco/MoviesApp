da8dafaa0eff151e4bd4d3ae7e100461ab5cca46
mikececco <mikececco2000@gmail.com>
2023-02-09 21:02:38 +0100

    Pushing correct version of: Added 'Go to store' button primary functions

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ let fighting;
 let monsterHealth;
 let inventory = ['stick'];
 
-const button1 = documentQuerySelector("#button1");
-const button2 = documentQuerySelector("#button2");
-const button3 = documentQuerySelector("#button3");
-const text = documentQuerySelector("#text");
-const xpText = documentQuerySelector("#xpText");
-const healthText = documentQuerySelector("#healthText");
-const goldText = documentQuerySelector("#goldText");
-const monsterStats = documentQuerySelector("#monsterStats");
-const monsterNameText = documentQuerySelector("#monsterName");
-const monsterHealthText = documentQuerySelector("#monsterHealth");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterNameText = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
 
 button1.onclick = goStore;
 button2.onclick = goCave;
