bfb9b2726c4a01770135cf9a24933d4ed025d645
mikececco <mikececco2000@gmail.com>
2023-02-09 21:26:55 +0100

    Refractored Go Store function and added connection between GoStore and GoTown

--- script.js
+++ script.js
@@ -17,18 +17,42 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterNameText = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [
+    {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: "You are in the town square. You see the sign that says \"store.\""
+    },
+    {
+    name: "store",
+    "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You entered the store"
+    }
+]
+
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function update(location) {
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = location.text;
+
+}
+
+function goTown() {
+    update(locations[0]);
+}
+
 function goStore() {
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
-    text.innerText = "You entered the store.";
+    update(locations[1]);
 }
 
 function goCave() {
@@ -47,9 +71,6 @@ function buyWeapon() {
 
 }
 
-function goTown() {
-
-}
 
 
 
